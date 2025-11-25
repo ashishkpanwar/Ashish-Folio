c3f1b6226c722bf32bc0577a95331045da525eb4
Ashish <ashishkpanwar@gmail.com>
2025-11-25 11:01:49 +0530

    just

--- Ashish-Backend-Folio/src/obj/Debug/net10.0/Ashish-Backend-Folio.AssemblyInfo.cs
+++ Ashish-Backend-Folio/src/obj/Debug/net10.0/Ashish-Backend-Folio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ashish-Backend-Folio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8cd06ec96a3ef73022b0cd232b991225065bcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc3abd36e912d7f890f139bca94f778c34bfbbb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ashish-Backend-Folio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ashish-Backend-Folio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
