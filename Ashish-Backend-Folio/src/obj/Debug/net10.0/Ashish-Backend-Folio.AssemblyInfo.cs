b7d6b3a14a4306ffe592692ceb2627da31053336
Ashish <ashishkpanwar@gmail.com>
2025-12-05 14:08:50 +0530

    updated build pipeline

--- Ashish-Backend-Folio/src/obj/Debug/net10.0/Ashish-Backend-Folio.AssemblyInfo.cs
+++ Ashish-Backend-Folio/src/obj/Debug/net10.0/Ashish-Backend-Folio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ashish-Backend-Folio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3f1b6226c722bf32bc0577a95331045da525eb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6dc1c62db8b23a42c7201e583a538a5bbc4ac1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ashish-Backend-Folio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ashish-Backend-Folio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
