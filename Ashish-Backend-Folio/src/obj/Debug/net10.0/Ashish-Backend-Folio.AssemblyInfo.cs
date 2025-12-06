7a96d9426b7eafa5f46d890da58cd8e850befec7
Ashish <ashishkpanwar@gmail.com>
2025-12-06 15:03:50 +0530

    updated project refe

--- Ashish-Backend-Folio/src/obj/Debug/net10.0/Ashish-Backend-Folio.AssemblyInfo.cs
+++ Ashish-Backend-Folio/src/obj/Debug/net10.0/Ashish-Backend-Folio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ashish-Backend-Folio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7d6b3a14a4306ffe592692ceb2627da31053336")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a188a0c0157969fed828220b6a12928fe17c30")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ashish-Backend-Folio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ashish-Backend-Folio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
